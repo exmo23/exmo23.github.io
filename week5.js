d9c68b7d01943176c1bddfdc8ebb96e842ff907b
exmo23 <143752889+exmo23@users.noreply.github.com>
2025-09-21 19:19:38 -0600

    interactive element

---
+++ week5.js
@@ -0,0 +1,14 @@
+ const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu'); 
+
+var menu_shown = false;
+
+function showMenu() {
+    var shown = navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+    if (shown) {
+        navToggle.setAttribute()
+    }
+}
+navToggle.addEventListener("click",)
