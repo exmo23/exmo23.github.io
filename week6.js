ad74a6b1b986bc747f03adce63af417d88767fd3
exmo23 <143752889+exmo23@users.noreply.github.com>
2025-10-15 23:31:27 -0600

    week 9

--- week6.js
+++ week6.js
@@ -5,6 +5,10 @@ const filtertop = document.querySelector("FilterByTop");
 const topListing = document.querySelector("Listing3");
 const darkThemeBtn = document.querySelector("darkThemeButton");
 const lightThemeBtn = document.querySelector("lightThemeButton");
+const thirteenlistings = document.querySelector(".age13");
+const sixteenlistings = document.querySelector(".age16");
+const eighteenlistings = document.querySelector(".age18");
+const twentyonelistings = document.querySelector(".age21");
 
 var menu_shown = false;
 
@@ -30,6 +34,69 @@ function sortByTop() {
     }
 }
 
+function sortBy13() {
+    thirteenlistings.classList.add("show");
+    if (thirteenlistings.classList.contains("hide")) {
+        thirteenlistings.classList.remove("hide");
+    }
+    sixteenlistings.classList.remove("show");
+    sixteenlistings.classList.add("hide");
+    eighteenlistings.classList.remove("show");
+    eighteenlistings.classList.add("hide");
+    twentyonelistings.classList.remove("show");
+    twentyonelistings.classList.add("hide");
+}
+
+function sortBy16() {
+    sixteenlistings.classList.add("show");
+    if (sixteenlistings.classList.contains("hide")) {
+        sixteenlistings.classList.remove("hide");
+    }
+    thirteenlistings.classList.remove("show");
+    thirteenlistings.classList.add("hide");
+    eighteenlistings.classList.remove("show");
+    eighteenlistings.classList.add("hide");
+    twentyonelistings.classList.remove("show");
+    twentyonelistings.classList.add("hide");
+}
+
+function sortBy18() {
+    eighteenlistings.classList.add("show");
+    if (eighteenlistings.classList.contains("hide")) {
+        eighteenlistings.classList.remove("hide");
+    }
+    thirteenlistings.classList.remove("show");
+    thirteenlistings.classList.add("hide");
+    sixteenlistings.classList.remove("show");
+    sixteenlistings.classList.add("hide");
+    twentyonelistings.classList.remove("show");
+    twentyonelistings.classList.add("hide");
+}
+
+function sortBy21() {
+    twentyonelistings.classList.add("show");
+    if (twentyonelistings.classList.contains("hide")) {
+        twentyonelistings.classList.remove("hide");
+    }
+    thirteenlistings.classList.remove("show");
+    thirteenlistings.classList.add("hide");
+    eighteenlistings.classList.remove("show");
+    eighteenlistings.classList.add("hide");
+    sixteenlistings.classList.remove("show");
+    sixteenlistings.classList.add("hide");
+}
+
+function clearFilter() {
+    thirteenlistings.classList.remove("hide");
+    thirteenlistings.classList.add("show");
+    sixteenlistings.classList.remove("hide");
+    sixteenlistings.classList.add("show");
+    eighteenlistings.classList.remove("hide");
+    eighteenlistings.classList.add("show");
+    twentyonelistings.classList.remove("hide");
+    twentyonelistings.classList.add("show");
+}
+
 // Save user"s theme choice
 function setTheme(theme) {
     localStorage.setItem("userTheme", theme);
@@ -41,9 +108,3 @@ window.addEventListener("load", function() {
     const savedTheme = localStorage.getItem("userTheme") || "light";
     document.body.className = savedTheme;
 });
-
-darkThemeBtn.addEventListener("click", setTheme("dark"));
-lightThemeBtn.addEventListener("click", setTheme("light"));
-
-navMenu.addEventListener("");
-filtertop.addEventListener("");
